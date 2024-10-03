5583b8032bff0918785802308dad686fb5ff3652
byatham <balaiahy23@gmail.com>
2024-10-03 17:47:36 +0530

    modified docker host

--- src/main/java/com/anu/consumer/fien/FienClientTemplate.java
+++ src/main/java/com/anu/consumer/fien/FienClientTemplate.java
@@ -1,4 +1,4 @@
-package com.anu.consumer.fien;
+ package com.anu.consumer.fien;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
